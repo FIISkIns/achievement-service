b27b725d0ba2e7755ec02b5ced18730363d3d03d
Alexandru Pomohaci <alexandrupomohaci@yahoo.com>
2018-06-06 22:15:46 +0300

    Initial commit

---
+++ config.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/kelseyhightower/envconfig"
+
+type ConfigurationSpec struct {
+	Port                     int    `default:"8004"`
+	CourseProgressServiceUrl string `default:"http://127.0.0.1:8003" envconfig:"PROGRESS_SERVICE_URL"`
+	StatsServiceUrl string `default:"http://127.0.0.1:8003" envconfig:"STATS_SERVICE_URL"`
+}
+
+var config ConfigurationSpec
+
+func initConfig() {
+	envconfig.MustProcess("achievement", &config)
+}
